e4f1afabe31a961b716d6a04c3d65007e0516db7
Jayant <jayant@kredily.com>
2019-12-27 17:02:25 +0530

    scroll magic

--- src/components/Header.js
+++ src/components/Header.js
@@ -10,28 +10,32 @@ if (typeof window !== `undefined`) {
 
 export default function Header(props) {
   let headerItem, mainLogo, logoEnglish, leftLinks = useRef(null)
-  let controller = new ScrollMagic.Controller();
+  if(ScrollMagic){
+    let controller = new ScrollMagic.Controller();
+  }
   useEffect(()=>{
-    let tl = new TimelineMax()
-    let headerShadow = tl.to(headerItem, 0.3,{
-      boxShadow:'0 4px 5px 0 rgba(0,0,0,0.14), 0 1px 10px 0 rgba(0,0,0,0.12), 0 2px 4px -1px rgba(0,0,0,0.3)',
-      background: 'red',
-      color:'#fff'
-    }).to(mainLogo, 0.3, {
-      color:'#fff',
-      delay: -0.3
-    }).to(logoEnglish, 0.3, {
-      color:'#2d0f0f',
-      delay: -0.3
-    }).to(leftLinks, 0.3,{
-      color:'#fff',
-      delay: -0.3
-    })
-    new ScrollMagic.Scene({
-      triggerElement: '.BlogArea',
-    })
-    .setTween(headerShadow)// pins the element for the the scene's duration
-    .addTo(controller);
+    if(ScrollMagic){
+      let tl = new TimelineMax()
+      let headerShadow = tl.to(headerItem, 0.3,{
+        boxShadow:'0 4px 5px 0 rgba(0,0,0,0.14), 0 1px 10px 0 rgba(0,0,0,0.12), 0 2px 4px -1px rgba(0,0,0,0.3)',
+        background: 'red',
+        color:'#fff'
+      }).to(mainLogo, 0.3, {
+        color:'#fff',
+        delay: -0.3
+      }).to(logoEnglish, 0.3, {
+        color:'#2d0f0f',
+        delay: -0.3
+      }).to(leftLinks, 0.3,{
+        color:'#fff',
+        delay: -0.3
+      })
+      new ScrollMagic.Scene({
+        triggerElement: '.BlogArea',
+      })
+      .setTween(headerShadow)// pins the element for the the scene's duration
+      .addTo(controller);
+    }
   })  
   return (
     <header ref={el => (headerItem = el)}
