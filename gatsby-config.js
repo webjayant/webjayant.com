f716c5d6e7c7d7e33bfb5622bbcf2a2f3b8d7c52
Jayant Raj Singh <jayant.singh123@gmail.com>
2022-03-27 14:04:04 +0530

    Added calrity

--- gatsby-config.js
+++ gatsby-config.js
@@ -74,5 +74,16 @@ module.exports = {
         trackingId: "UA-175457215-1",
       },
     },
+    {
+        resolve: `gatsby-plugin-clarity`,
+        options: {
+          // String value for your clarity project ID
+          clarity_project_id: b7iyy9r65t,
+          // Boolean value for enabling clarity while developing
+          // true will enable clarity tracking code on both development and production environments
+          // false will enable clarity tracking code on production environment only
+          enable_on_dev_env: false
+        },
+    }
   ],
 }
